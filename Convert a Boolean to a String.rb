05032173c453a9ef30b5c099b7f23037fed7620f
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-09-04 18:19:35 -0500

    Create Convert a Boolean to a String.rb

---
+++ Convert a Boolean to a String.rb
@@ -0,0 +1,15 @@
+def boolean_to_string(b)
+  if b == true
+    return "true"
+  else
+    return "false"
+  end
+end
+
+#def boolean_to_string(b)
+#  b.to_s
+#end
+
+#def boolean_to_string(b)
+#  b ? "true" : "false"
+#end
