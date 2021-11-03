a26061b74b03b4d563c45458eca6f227b4d34fe2
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-02 21:26:33 -0500

    Update Is n divisible by x and y.rb

--- Is n divisible by x and y.rb
+++ Is n divisible by x and y.rb
@@ -1,7 +1,11 @@
+#def is_divisible(n,x,y)
+#  if n % x === 0 && n % y === 0
+#    return true
+#  else
+#    return false
+#  end
+#end
+
 def is_divisible(n,x,y)
-  if n % x === 0 && n % y === 0
-    return true
-  else
-    return false
-  end
+  (n % x == 0) && (n % y == 0)
 end
