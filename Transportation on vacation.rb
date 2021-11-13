0026d85e51c1c39a473d4430850a17f49ade520b
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-13 12:42:07 -0600

    Update Transportation on vacation.rb

--- Transportation on vacation.rb
+++ Transportation on vacation.rb
@@ -1,9 +1,10 @@
 def rental_car_cost(d)
   rental = d * 40
-  if d = 3..6
-    return rental - 30
-  elsif d >= 7
-    return rental - 50
-  else
+  if d < 3
     return rental
+  elsif d <= 6
+    return rental - 20
+  else
+    return rental - 50
+  end
 end
