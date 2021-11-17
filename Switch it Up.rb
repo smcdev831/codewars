2f1879d9598408daa42507bcc2f0cabe6b858a30
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-17 11:59:03 -0600

    Create Switch it Up.rb

---
+++ Switch it Up.rb
@@ -0,0 +1,24 @@
+def switch_it_up(number)
+  case number
+  when 0
+    return "Zero"
+  when 1
+    return "One"
+  when 2
+    return "Two"
+  when 3
+    return "Three"
+  when 4
+    return "Four"
+  when 5
+    return "Five"
+  when 6
+    return "Six"
+  when 7
+    return "Seven"
+  when 8
+    return "Eight"
+  when 9
+    return "Nine"
+  end
+end
