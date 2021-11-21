742b1eb048eac1ecba2b63c6baa2e084276af969
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-11-21 13:39:03 -0600

    Update Is there a vowel in there.rb

--- Is there a vowel in there.rb
+++ Is there a vowel in there.rb
@@ -1,3 +1,7 @@
+# def is_vow(a)
+#   a.each.with_index {|x,y| a[y] = x.chr if 'aeiou'.include? x.chr}
+# end
+
 def is_vow(a)
-  a.each.with_index {|x,y| a[y] = x.chr if 'aeiou'.include? x.chr}
+  a.map { |l| l.chr =~ /[aeoui]/ ? l.chr : l }
 end
