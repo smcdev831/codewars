4528201a17ee5bfe94569d807d76a4bdb430895a
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-01-02 21:56:37 -0600

    Create Not Completed - Count the days.js

---
+++ Not Completed - Count the days.js
@@ -0,0 +1,11 @@
+function countDays(d){
+  var today = new Date();
+  if d == today
+    return "Today is the day!"
+  else if d < Today
+    return "The day is in the past!"
+  else
+    sum = today - d
+    return "{sum} days"
+  end
+}
