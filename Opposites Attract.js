2bb89607037b9fc93574bb47f0c7eab6b1ddfab1
smcdev <smcdev@outlook.com>
2022-05-30 23:39:38 -0500

    Create Opposites Attract.js

---
+++ Opposites Attract.js
@@ -0,0 +1,17 @@
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 === 0) {
+    return false;
+  } else if (flower1 % 2 != 0 && flower2 % 2 != 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//refactoring
+
+
+function lovefunc(flower1, flower2) {
+    if (flower1 + flower2 % 2 != 1) {
+        return false
+  }
