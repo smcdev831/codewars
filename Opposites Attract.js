6f4afc52345737c5dbac305d6fc137313fbe135c
smcdev <smcdev@outlook.com>
2022-05-30 23:43:39 -0500

    Update Opposites Attract.js

--- Opposites Attract.js
+++ Opposites Attract.js
@@ -11,7 +11,5 @@ function lovefunc(flower1, flower2) {
 //refactoring
 
 function lovefunc(flower1, flower2) {
-  if ((flower1 + flower2) % 2 != 1) {
-    return true;
-  }
+  return (flower1 + flower2) % 2 === 1;
 }
