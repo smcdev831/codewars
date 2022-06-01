a7cedb9786c5ef634850d490bb166b7559c06874
smcdev <smcdev@outlook.com>
2022-05-31 21:15:01 -0500

    Update Is it a palindrome.js

--- Is it a palindrome.js
+++ Is it a palindrome.js
@@ -2,3 +2,11 @@ function isPalindrome(x) {
   let reversed = x.toLowerCase().split("").reverse().join("");
   return reversed === x.toLowerCase() ? true : false;
 }
+
+//refactoring
+
+function isPalindrome(x) {
+  return x.toLowerCase().split("").reverse().join("") === x.toLowerCase()
+    ? true
+    : false;
+}
