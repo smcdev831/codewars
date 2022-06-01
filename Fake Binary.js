a9bdfb63cfbd563db11013158413d75ba7e00f22
smcdev <smcdev@outlook.com>
2022-06-01 17:00:57 -0500

    Create Fake Binary.js

---
+++ Fake Binary.js
@@ -0,0 +1,11 @@
+function fakeBin(x) {
+  let arr = "";
+  for (let i = 0; i < x.length; i++) {
+    if (Number(x[i]) >= 5) {
+      arr += 1;
+    } else {
+      arr += 0;
+    }
+  }
+  return arr;
+}
