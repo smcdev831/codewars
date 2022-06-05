32868ea9e7a4082e4a9ac94836291c50028d29f3
smcdev <smcdev@outlook.com>
2022-06-05 15:32:36 -0500

    Update Area or Perimeter.js

--- Area or Perimeter.js
+++ Area or Perimeter.js
@@ -11,3 +11,8 @@ const areaOrPerimeter = function (l, w) {
 const areaOrPerimeter = function (l, w) {
   return l === w ? l * w : l * 2 + w * 2;
 };
+
+//refactoring
+const areaOrPerimeter = function (l, w) {
+  return l === w ? l * w : 2 * (l + w);
+};
