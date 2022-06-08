bdd4ae85ba6f631cc0e573168ce6082b703a75fc
smcdev <smcdev@outlook.com>
2022-06-07 22:36:02 -0500

    Update Switch it Up.js

--- Switch it Up.js
+++ Switch it Up.js
@@ -74,3 +74,28 @@ function switchItUp(number) {
       break;
   }
 }
+
+//refactoring
+function switchItUp(number) {
+  return number === 0
+    ? "Zero"
+    : number === 1
+    ? "One"
+    : number === 2
+    ? "Two"
+    : number === 3
+    ? "Three"
+    : number === 4
+    ? "Four"
+    : number === 5
+    ? "Five"
+    : number === 6
+    ? "Six"
+    : number === 7
+    ? "Seven"
+    : number === 8
+    ? "Eight"
+    : number === 9
+    ? "Nine"
+    : "This is not a number between 0 and 9";
+}
