c83c4295ecd9effec350950fb9f62b74ad3c31cb
smcdev <smcdev@outlook.com>
2022-06-08 00:08:28 -0500

    Update Find the Difference in Age between Oldest and Youngest Family Members.js

--- Find the Difference in Age between Oldest and Youngest Family Members.js
+++ Find the Difference in Age between Oldest and Youngest Family Members.js
@@ -1,7 +1,6 @@
 function differenceInAges(ages) {
-  let ageRange = [];
-  let oldest = Math.max(ages);
-  let youngest = Math.min(ages);
+  let oldest = Math.max(...ages);
+  let youngest = Math.min(...ages);
   let difference = oldest - youngest;
-  return ageRange.push(youngest, oldest, difference);
+  return [youngest, oldest, difference];
 }
