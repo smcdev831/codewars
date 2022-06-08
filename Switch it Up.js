ae337999a24a65fb931875f71fd1e0e829bf4eb6
smcdev <smcdev@outlook.com>
2022-06-07 22:28:33 -0500

    Create Switch it Up.js

---
+++ Switch it Up.js
@@ -0,0 +1,37 @@
+function switchItUp(number) {
+  switch (true) {
+    case number === 0:
+      return "Zero";
+      break;
+    case number === 1:
+      return "One";
+      break;
+    case number === 2:
+      return "Two";
+      break;
+    case number === 3:
+      return "Three";
+      break;
+    case number === 4:
+      return "Four";
+      break;
+    case number === 5:
+      return "Five";
+      break;
+    case number === 6:
+      return "Six";
+      break;
+    case number === 7:
+      return "Seven";
+      break;
+    case number === 8:
+      return "Eight";
+      break;
+    case number === 9:
+      return "Nine";
+      break;
+    default:
+      return "This isn't a number between 0 and 9";
+      break;
+  }
+}
