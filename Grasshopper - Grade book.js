78637b77dcf3d85d24333ad45a76531e2916eca4
smcdev <smcdev@outlook.com>
2022-06-07 21:47:43 -0500

    Update Grasshopper - Grade book.js

--- Grasshopper - Grade book.js
+++ Grasshopper - Grade book.js
@@ -12,3 +12,25 @@ function getGrade(s1, s2, s3) {
     return "F";
   }
 }
+
+//refactoring
+function getGrade(s1, s2, s3) {
+  let average = (s1 + s2 + s3) / 3;
+  switch (true) {
+    case average >= 90:
+      return "A";
+      break;
+    case average >= 80:
+      return "B";
+      break;
+    case average >= 70:
+      return "C";
+      break;
+    case average >= 60:
+      return "D";
+      break;
+    default:
+      return "F";
+      break;
+  }
+}
