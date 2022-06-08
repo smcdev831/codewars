e95b915caeacaea0cef4158c8d9fbecb46ff92b4
smcdev <smcdev@outlook.com>
2022-06-08 00:15:01 -0500

    Update Get Planet Name By ID.js

--- Get Planet Name By ID.js
+++ Get Planet Name By ID.js
@@ -52,3 +52,18 @@ function getPlanetName(id) {
 
   return name;
 }
+
+//refactoring
+
+function getPlanetName(id) {
+  return {
+    1: "Mercury",
+    2: "Venus",
+    3: "Earth",
+    4: "Mars",
+    5: "Jupiter",
+    6: "Saturn",
+    7: "Uranus",
+    8: "Neptune",
+  }[id];
+}
