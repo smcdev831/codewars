1e310853092a01f429269802bf2c83c9c889d9c9
smcdev <smcdev@outlook.com>
2022-06-12 18:18:38 -0500

    Update How many stairs will Suzuki climb in 20 years.js

--- How many stairs will Suzuki climb in 20 years.js
+++ How many stairs will Suzuki climb in 20 years.js
@@ -3,3 +3,13 @@ function stairsIn20(s) {
   var days = [].concat(...year);
   return days.reduce((a, b) => a + b) * 20;
 }
+
+//refactoring
+
+function stairsIn20(s) {
+  let days = 0;
+  for (let i = 0; i < s.length; i++) {
+    days += s[i].reduce((a, b) => a + b);
+  }
+  return 20 * days;
+}
