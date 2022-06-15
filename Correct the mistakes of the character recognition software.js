e4ea781cef9711a3924681bd5910aa87c2724e40
smcdev <smcdev@outlook.com>
2022-06-14 22:47:54 -0500

    Update Correct the mistakes of the character recognition software.js

--- Correct the mistakes of the character recognition software.js
+++ Correct the mistakes of the character recognition software.js
@@ -1,3 +1,19 @@
 function correct(string) {
   return string.split("0").join("O").split("1").join("I").split("5").join("S");
 }
+
+//refactoring
+
+function correct(string) {
+  newString = string.split("");
+  for (let i = 0; i < newString.length; i++) {
+    if (newString[i] === "0") {
+      newString[i] = "O";
+    } else if (newString[i] === "1") {
+      newString[i] = "I";
+    } else if (newString[i] === "5") {
+      newString[i] = "S";
+    }
+  }
+  return newString.join("");
+}
