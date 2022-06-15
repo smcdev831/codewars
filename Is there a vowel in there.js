7d4db9896b6aeb7165dd26f8745cca900105288c
smcdev <smcdev@outlook.com>
2022-06-14 23:07:40 -0500

    Create Is there a vowel in there.js

---
+++ Is there a vowel in there.js
@@ -0,0 +1,10 @@
+function isVow(a) {
+  return a.map((i) => {
+    if (String.fromCharCode(i) === "a") return "a";
+    else if (String.fromCharCode(i) === "e") return "e";
+    else if (String.fromCharCode(i) === "i") return "i";
+    else if (String.fromCharCode(i) === "o") return "o";
+    else if (String.fromCharCode(i) === "u") return "u";
+    else return i;
+  });
+}
