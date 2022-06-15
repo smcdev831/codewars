5858c730751d42840ba8ed60d1a9441d4a34e809
smcdev <smcdev@outlook.com>
2022-06-14 23:14:56 -0500

    Update Calculate average.js

--- Calculate average.js
+++ Calculate average.js
@@ -2,3 +2,10 @@ function find_average(array) {
   let average = array.reduce((a, b) => a + b, 0) / array.length;
   return average || 0;
 }
+
+//refactoring
+function find_average(array) {
+  return array.length === 0
+    ? 0
+    : array.reduce((a, b) => a + b, 0) / array.length;
+}
