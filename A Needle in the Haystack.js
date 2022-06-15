47edabc11022590180b2be8694e05405ea36ea6b
smcdev <smcdev@outlook.com>
2022-06-14 22:10:51 -0500

    Update A Needle in the Haystack.js

--- A Needle in the Haystack.js
+++ A Needle in the Haystack.js
@@ -2,3 +2,8 @@ function findNeedle(haystack) {
   location = haystack.indexOf("needle");
   return `found the needle at position ${location}`;
 }
+
+//refactoring
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
