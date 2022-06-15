65ab18b630fe3f9fe7ee38d49161f13e5de9a570
smcdev <smcdev@outlook.com>
2022-06-14 22:06:53 -0500

    Update Abbreviate a Two Word Name.js

--- Abbreviate a Two Word Name.js
+++ Abbreviate a Two Word Name.js
@@ -4,3 +4,10 @@ function abbrevName(name) {
   lastName = fullName[1].toUpperCase();
   return `${firstName.slice(0, 1)}.${lastName.slice(0, 1)}`;
 }
+
+//refactoring
+
+function abbrevName(name) {
+  fullName = name.toUpperCase().split(" ");
+  return `${fullName[0][0]}.${fullName[1][0]}`;
+}
