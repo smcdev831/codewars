f4f3a9fcf95f065329f676cfc084c864256ca54f
smcdev <smcdev@outlook.com>
2022-06-16 01:52:31 -0500

    Update Total amount of points.js

--- Total amount of points.js
+++ Total amount of points.js
@@ -1,6 +1,22 @@
 function points(games) {
   let totalPoints = 0;
   for (let i = 0; i < games.length; i++) {
+    let score = games[i].split(":");
+    if (score[0] > score[1]) {
+      totalPoints += 3;
+    } else if (score[0] == score[1]) {
+      totalPoints += 1;
+    } else {
+      totalPoints = totalPoints;
+    }
+  }
+  return totalPoints;
+}
+
+//refactoring
+function points(games) {
+  let totalPoints = 0;
+  for (let i = 0; i < games.length; i++) {
     if (games[i][0] > games[i][2]) totalPoints += 3;
     if (games[i][0] == games[i][2]) totalPoints += 1;
   }
