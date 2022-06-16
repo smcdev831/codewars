7cf07cae47547c0de1748f7d82466c91c50e9e5c
smcdev <smcdev@outlook.com>
2022-06-16 02:13:56 -0500

    Update Multiplication table for number.js

--- Multiplication table for number.js
+++ Multiplication table for number.js
@@ -1,3 +1,16 @@
+let multiTable = (number) => {
+  return `1 * ${number} = ${1 * number}\n2 * ${number} = ${
+    2 * number
+  }\n3 * ${number} = ${3 * number}\n4 * ${number} = ${
+    4 * number
+  }\n5 * ${number} = ${5 * number}\n6 * ${number} = ${
+    6 * number
+  }\n7 * ${number} = ${7 * number}\n8 * ${number} = ${
+    8 * number
+  }\n9 * ${number} = ${9 * number}\n10 * ${number} = ${10 * number}`;
+};
+
+//refactoring
 function multiTable(number) {
   let list = "";
   for (let i = 1; i <= 10; i++) {
