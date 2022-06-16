3b6804f92088c0e6cb6698607c4a0b15fadea825
smcdev <smcdev@outlook.com>
2022-06-16 01:50:25 -0500

    Create Total amount of points.js

---
+++ Total amount of points.js
@@ -0,0 +1,8 @@
+function points(games) {
+  let totalPoints = 0;
+  for (let i = 0; i < games.length; i++) {
+    if (games[i][0] > games[i][2]) totalPoints += 3;
+    if (games[i][0] == games[i][2]) totalPoints += 1;
+  }
+  return totalPoints;
+}
