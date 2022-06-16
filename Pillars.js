0c78e151cc725285ef0c318bb2df86cd599ba232
smcdev <smcdev@outlook.com>
2022-06-16 01:35:29 -0500

    Update Pillars.js

--- Pillars.js
+++ Pillars.js
@@ -5,3 +5,10 @@ function pillars(num_pill, dist, width) {
     return dist * 100 * (num_pill - 1) + width * (num_pill - 2);
   }
 }
+
+//refactoring
+function pillars(num_pill, dist, width) {
+  return num_pill === 1
+    ? 0
+    : dist * 100 * (num_pill - 1) + width * (num_pill - 2);
+}
