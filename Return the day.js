d8d5818327b4f20f807691eb5bbcf4be279e4953
smcdev <smcdev@outlook.com>
2022-06-18 20:44:45 -0500

    Update Return the day.js

--- Return the day.js
+++ Return the day.js
@@ -3,7 +3,24 @@ function whatday(num) {
     case 1:
       return "Sunday";
       break;
-
+    case 2:
+      return "Monday";
+      break;
+    case 3:
+      return "Tuesday";
+      break;
+    case 4:
+      return "Wednesday";
+      break;
+    case 5:
+      return "Thursday";
+      break;
+    case 6:
+      return "Friday";
+      break;
+    case 7:
+      return "Saturday";
+      break;
     default:
       return "Wrong, please enter a number between 1 and 7";
       break;
