e7cde22d62b8dc95f2668363e1144b28c4174045
smcdev <smcdev@outlook.com>
2022-06-18 20:43:38 -0500

    Create Return the day.js

---
+++ Return the day.js
@@ -0,0 +1,11 @@
+function whatday(num) {
+  switch (num) {
+    case 1:
+      return "Sunday";
+      break;
+
+    default:
+      return "Wrong, please enter a number between 1 and 7";
+      break;
+  }
+}
