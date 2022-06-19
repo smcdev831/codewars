6855218cab128669cc5d012ca228ca0b526189f7
smcdev <smcdev@outlook.com>
2022-06-18 20:49:27 -0500

    Update Return the day.js

--- Return the day.js
+++ Return the day.js
@@ -28,3 +28,15 @@ function whatday(num) {
 }
 
 //refactoring
+function whatday(num) {
+  let days = {
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday",
+  };
+  return days[num] || "Wrong, please enter a number between 1 and 7";
+}
