65298d24971f3119b75e219ee093f3a53f291635
smcdev <smcdev@outlook.com>
2022-07-06 20:13:54 -0500

    Create For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js

---
+++ For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,5 @@
+var quote = function (fighter) {
+  return fighter.toUpperCase() == "GEORGE SAINT PIERRE"
+    ? "I am not impressed by your performance."
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
