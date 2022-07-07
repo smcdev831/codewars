059a68eef611da74144a01c15213fc64147d5e3e
smcdev <smcdev@outlook.com>
2022-07-07 17:14:21 -0500

    Update Blood-Alcohol Content.js

--- Blood-Alcohol Content.js
+++ Blood-Alcohol Content.js
@@ -1,1 +1,7 @@
-function bloodAlcoholContent(drinks, weight, sex, time) {}
+function bloodAlcoholContent(drinks, weight, sex, time) {
+  if (sex === "female") {
+    return (drinks * 5.14) / (weight * 0.66) - 0.015 * time;
+  } else {
+    return (drinks * 5.14) / (weight * 0.73) - 0.015 * time;
+  }
+}
