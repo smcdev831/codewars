8770ccb5ec9edf2e17ebeb056ac15300afabffaa
smcdev <smcdev@outlook.com>
2022-07-07 17:28:24 -0500

    Update Blood-Alcohol Content.js

--- Blood-Alcohol Content.js
+++ Blood-Alcohol Content.js
@@ -4,5 +4,5 @@ function bloodAlcoholContent(drinks, weight, sex, time) {
   } else {
     bac = ((drinks.ounces * drinks.abv * 5.14) / weight) * 0.73 - 0.015 * time;
   }
-  return bac.toFixed(4);
+  return Number(bac.toFixed(4));
 }
