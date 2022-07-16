c93954d0a51e32b1085e2cde70d66eb731012912
smcdev <smcdev@outlook.com>
2022-07-16 16:50:03 -0500

    Update Welcome.js

--- Welcome.js
+++ Welcome.js
@@ -1,1 +1,23 @@
-function greet(language) {}
+function greet(language) {
+  return languages[language] || languages["english"];
+}
+
+var languages = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso",
+};
