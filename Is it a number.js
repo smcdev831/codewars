110fa32a3da43db600110a6621729dc9a1698e94
smcdev <smcdev@outlook.com>
2022-07-18 17:02:05 -0500

    Update Is it a number.js

--- Is it a number.js
+++ Is it a number.js
@@ -1,1 +1,11 @@
-function isDigit(s) {}
+function isDigit(s) {
+  if (s === " ") {
+    return false;
+  } else if (s === "") {
+    return false;
+  } else if (s > 0 || s <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
