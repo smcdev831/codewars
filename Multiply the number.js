c99696d453e3333167ec7152536dab445b8b06e3
smcdev <smcdev@outlook.com>
2022-07-19 16:30:07 -0500

    Update Multiply the number.js

--- Multiply the number.js
+++ Multiply the number.js
@@ -2,3 +2,8 @@ function multiply(number) {
   let count = Math.abs(number).toString().length;
   return number * Math.pow(5, count);
 }
+
+//refactoring
+function multiply(number) {
+  return number * Math.pow(5, Math.abs(number).toString().length);
+}
