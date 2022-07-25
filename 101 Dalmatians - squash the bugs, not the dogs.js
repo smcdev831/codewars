dceeea7ba1453ca7e7b50af0e488d1af528662fd
smcdev <smcdev@outlook.com>
2022-07-25 17:42:56 -0500

    Update 101 Dalmatians - squash the bugs, not the dogs.js

--- 101 Dalmatians - squash the bugs, not the dogs.js
+++ 101 Dalmatians - squash the bugs, not the dogs.js
@@ -51,3 +51,14 @@ function howManyDalmatians(number) {
 
   return respond;
 }
+
+//refactoring
+function howManyDalmatians(number) {
+  return number <= 10
+    ? "Hardly any"
+    : number <= 50
+    ? "More than a handful!"
+    : number === 101
+    ? "101 DALMATIANS!!!"
+    : "Woah that's a lot of dogs!";
+}
