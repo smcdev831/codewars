05978f35779e5cc116e54687d02f3f305c9dce5b
smcdev <smcdev@outlook.com>
2022-07-28 18:14:34 -0500

    Update Leonardo Dicaprio and Oscars.js

--- Leonardo Dicaprio and Oscars.js
+++ Leonardo Dicaprio and Oscars.js
@@ -9,3 +9,14 @@ function leo(oscar) {
     return "When will you give Leo an Oscar?";
   }
 }
+
+//refactoring
+function leo(oscar) {
+  return oscar === 88
+    ? "Leo finally won the oscar! Leo is happy"
+    : oscar === 86
+    ? "Not even for Wolf of wallstreet?!"
+    : oscar > 88
+    ? "Leo got one already!"
+    : "When will you give Leo an Oscar?";
+}
