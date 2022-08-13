a8088ae4237086e2f0db62204f4a77d7947055bb
smcdev <smcdev@outlook.com>
2022-08-12 21:25:00 -0500

    Update Be Concise IV - Index of an element in an array.js

--- Be Concise IV - Index of an element in an array.js
+++ Be Concise IV - Index of an element in an array.js
@@ -1,3 +1,12 @@
 function find(array, element) {
   return array.includes(element) ? array.indexOf(element) : "Not found";
 }
+
+function find(array, element) {
+  val = array.indexOf(element);
+  if (val === -1) {
+    return "Not found";
+  } else {
+    return val;
+  }
+}
