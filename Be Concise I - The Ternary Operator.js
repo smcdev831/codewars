df30dfd78c8518bcd9741e7f21fbc33284c3d49c
smcdev <smcdev@outlook.com>
2022-08-13 21:34:16 -0500

    Update Be Concise I - The Ternary Operator.js

--- Be Concise I - The Ternary Operator.js
+++ Be Concise I - The Ternary Operator.js
@@ -19,3 +19,13 @@ function describeAge(age) {
     ? "You're a(n) adult"
     : "You're a(n) elderly";
 }
+
+function describeAge(a) {
+  return a <= 12
+    ? "You're a(n) kid"
+    : a >= 13 && a <= 17
+    ? "You're a(n) teenager"
+    : a >= 18 && a <= 64
+    ? "You're a(n) adult"
+    : "You're a(n) elderly";
+}
