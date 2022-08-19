581192ad98be996370606c7b6c64b4d9aefcaeea
smcdev <smcdev@outlook.com>
2022-08-19 14:31:21 -0500

    Create Training JS 2 Basic data types--Number.js

---
+++ Training JS 2 Basic data types--Number.js
@@ -0,0 +1,28 @@
+var v1 = 50;
+(v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
+function equal1() {
+  var a = v1;
+  var b = v1;
+  return a + b;
+}
+//Please refer to the example above to complete the following functions
+function equal2() {
+  var a = 200; //set number value to a
+  var b = 100; //set number value to b
+  return a - b;
+}
+function equal3() {
+  var a = 25; //set number value to a
+  var b = 4; //set number value to b
+  return a * b;
+}
+function equal4() {
+  var a = 100; //set number value to a
+  var b = 1; //set number value to b
+  return a / b;
+}
+function equal5() {
+  var a = 700; //set number value to a
+  var b = 200; //set number value to b
+  return a % b;
+}
