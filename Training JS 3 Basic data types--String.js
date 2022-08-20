42bb11933f81abf298cf42983fdc37d5d47fe87d
smcdev <smcdev@outlook.com>
2022-08-20 14:37:50 -0500

    Update Training JS 3 Basic data types--String.js

--- Training JS 3 Basic data types--String.js
+++ Training JS 3 Basic data types--String.js
@@ -1,28 +1,38 @@
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
-function Dad(){
+var a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+function Dad() {
   //select some variable to combine "Dad"
-  return ?+?+?;
+  return d1 + a2 + d2;
 }
-function Bee(){
+function Bee() {
   //select some variable to combine "Bee"
-  return ?+?+?;
+  return b1 + e2 + e2;
 }
-function banana(){
+function banana() {
   //select some variable to combine "banana"
-  return ?+?+?;
+  return b2 + a2 + n2 + a2 + n2 + a2;
 }
 
 //answer some questions if you finished works above
-function answer1(){
+function answer1() {
   //the answer should be "yes" or "no"
-  return "";
+  return "yes";
 }
-function answer2(){
+function answer2() {
   //the answer should be "yes" or "no"
-  return "";
+  return "no";
 }
-function answer3(){
+function answer3() {
   //the answer should be "yes" or "no"
-  return "";
+  return "yes";
 }
-
