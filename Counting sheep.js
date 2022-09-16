e1a328e15d160426726e44ebb89e56841dbc4b90
smcdev <smcdev@outlook.com>
2022-09-15 23:34:32 -0500

    Create Counting sheep.js

---
+++ Counting sheep.js
@@ -0,0 +1,9 @@
+function countSheeps(arrayOfSheep) {
+  let sheep = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      sheep += 1;
+    }
+  }
+  return sheep;
+}
