bf249d92c28b4987cf5402b536cf1706d6b99311
smcdev <smcdev@outlook.com>
2022-09-18 21:40:29 -0500

    Update No oddities here.js

--- No oddities here.js
+++ No oddities here.js
@@ -1,3 +1,9 @@
 function noOdds(values) {
   let evens = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] % 2 === 0) {
+      evens.push(values[i]);
+    }
+  }
+  return evens;
 }
