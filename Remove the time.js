dbfb4fa84e1e6e5106a4e228e9525203c1ff48a5
smcdev <smcdev@outlook.com>
2022-09-22 22:15:54 -0500

    Update Remove the time.js

--- Remove the time.js
+++ Remove the time.js
@@ -2,3 +2,8 @@ function shortenToDate(longDate) {
   let noTime = longDate.split(",");
   return noTime[0].toString();
 }
+
+//refactoring
+function shortenToDate(longDate) {
+  return longDate.split(",")[0];
+}
