7b31a645ba1a747393d42fbe1146b5a59879daca
smcdev <smcdev@outlook.com>
2022-10-24 01:08:22 -0500

    Create Training JS 21 Methods of String object--trim() and the string template.js

---
+++ Training JS 21 Methods of String object--trim() and the string template.js
@@ -0,0 +1,4 @@
+function fiveLine(s) {
+  let seven = s.trim();
+  return `${seven}\n${seven}${seven}\n${seven}${seven}${seven}\n${seven}${seven}${seven}${seven}\n${seven}${seven}${seven}${seven}${seven}`;
+}
