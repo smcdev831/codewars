4577a4a81aa3ba63e55413c9b33fd96610db8d14
smcdev <smcdev@outlook.com>
2022-11-13 19:24:36 -0600

    Update altERnaTIng cAsE  ALTerNAtiNG CaSe.js

--- altERnaTIng cAsE  ALTerNAtiNG CaSe.js
+++ altERnaTIng cAsE  ALTerNAtiNG CaSe.js
@@ -1,1 +1,13 @@
-String.prototype.toAlternatingCase = function () {};
+String.prototype.toAlternatingCase = function () {
+  let letters = string.split();
+  let alternates = [];
+  for (let i = 0; i < array.length; i++) {
+    if (words[i] === words[i].toUpperCase()) {
+      words[i].toLowerCase();
+      alternates.push(words[i]);
+    } else {
+      words[i].toUpperCase();
+      alternates.push(words[i]);
+    }
+  }
+};
