d06c0d2a33aec6b383901326f616e2c2749957f5
smcdev <smcdev@outlook.com>
2022-11-13 19:25:16 -0600

    Update altERnaTIng cAsE  ALTerNAtiNG CaSe.js

--- altERnaTIng cAsE  ALTerNAtiNG CaSe.js
+++ altERnaTIng cAsE  ALTerNAtiNG CaSe.js
@@ -1,13 +1,5 @@
 String.prototype.toAlternatingCase = function () {
-  let letters = string.split();
-  let alternates = [];
-  for (let i = 0; i < array.length; i++) {
-    if (words[i] === words[i].toUpperCase()) {
-      words[i].toLowerCase();
-      alternates.push(words[i]);
-    } else {
-      words[i].toUpperCase();
-      alternates.push(words[i]);
-    }
-  }
+  return this.split("")
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join("");
 };
