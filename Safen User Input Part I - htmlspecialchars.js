04700be5014b559b4a811c050f53d7521e87c9e4
smcdev <smcdev@outlook.com>
2022-11-15 19:43:04 -0600

    Update Safen User Input Part I - htmlspecialchars.js

--- Safen User Input Part I - htmlspecialchars.js
+++ Safen User Input Part I - htmlspecialchars.js
@@ -1,6 +1,17 @@
 function htmlspecialchars(formData) {
-  formData.replace("<", "&lt;");
-  formData.replace(">", "&gt;");
-  formData.replace(`"`, "&quot;");
-  formData.replace("&", "&amp;");
+  let corrected = "";
+  for (let i = 0; i < formData.length; i++) {
+    if (formData[i] === "<") {
+      corrected += "&lt;";
+    } else if (formData[i] === ">") {
+      corrected += "&gt;";
+    } else if (formData[i] === `"`) {
+      corrected += "&quot;";
+    } else if (formData[i] === "&") {
+      corrected += "&amp;";
+    } else {
+      corrected += formData[i];
+    }
+  }
+  return corrected;
 }
