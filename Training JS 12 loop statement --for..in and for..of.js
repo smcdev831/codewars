2b259755080b6f5953a999c06bebbfc4441f130e
smcdev <smcdev@outlook.com>
2022-11-28 17:22:17 -0600

    Update Training JS 12 loop statement --for..in and for..of.js

--- Training JS 12 loop statement --for..in and for..of.js
+++ Training JS 12 loop statement --for..in and for..of.js
@@ -1,15 +1,8 @@
 function giveMeFive(obj) {
-  let arr = [];
-  for (const key in obj) {
-    if (key.length >= 5) {
-      arr.push(obj[key]);
-    }
-  }
-
-  for (const value in obj) {
-    if (value.length >= 5) {
-      arr.push(obj[value]);
-    }
+  var arr = [];
+  for (var key in obj) {
+    if (key.length == 5) arr.push(key);
+    if (obj[key].length == 5) arr.push(obj[key]);
   }
   return arr;
 }
