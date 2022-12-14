180ce5a623baa3cd12f7f82fec6ebce1ecf1e4dd
smcdev <smcdev@outlook.com>
2022-12-13 19:15:46 -0600

    Update Playing with cubes II.js

--- Playing with cubes II.js
+++ Playing with cubes II.js
@@ -1,10 +1,13 @@
 // This Cube class needs help
 // Implement the constructor so that it can take an integer for the side or no args
 class Cube {
+  constructor(n) {
+    this.setSide(n);
+  }
   getSide() {
     return this.side || 0;
   }
-  setSide(n, 0) {
+  setSide(n) {
     this.side = Math.abs(n);
   }
 }
