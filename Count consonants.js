6ac9ccaffd7ca4b2f4b54c5b5ece328a42544cb5
smcdev <smcdev@outlook.com>
2023-01-15 00:10:28 -0600

    Update Count consonants.js

--- Count consonants.js
+++ Count consonants.js
@@ -1,3 +1,15 @@
 function consonantCount(str) {
   return (str.match(/[b-df-hj-np-tv-z]/gi) || []).length;
 }
+
+//Different solution
+function consonantCount(str) {
+  let arr = [];
+  let consonants = "bcdfghjklmnpqrstvwxzy";
+  for (let i = 0; i < str.length; i++) {
+    if (consonants.includes(str[i].toLowerCase)) {
+      arr.push(str[i]);
+    }
+  }
+  return arr.length;
+}
