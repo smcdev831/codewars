6d382d97aad7f2dbc96bd323652d480f5671fc3b
smcdev <smcdev@outlook.com>
2023-02-04 19:05:39 -0600

    Update You Complete Me.js

--- You Complete Me.js
+++ You Complete Me.js
@@ -1,5 +1,5 @@
 const complete = (str) => {
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 1; i <= str.length; i++) {
     if (str.slice(i) == [...str.slice(i)].reverse().join(""))
       return str + [...str.slice(0, i)].reverse().join("");
   }
