36ee72dfd89522ae383b19aef3dc3cf55881d95c
smcdev <smcdev@outlook.com>
2023-05-27 18:49:53 -0500

    Create Determine offspring sex based on genes XX and XY chromosomes.js

---
+++ Susurrate831/Determine offspring sex based on genes XX and XY chromosomes.js
@@ -0,0 +1,7 @@
+function chromosomeCheck(sperm) {
+  if (sperm === "XX") {
+    return "Congratulations! You're going to have a daughter.";
+  } else {
+    return "Congratulations! You're going to have a son.";
+  }
+}
