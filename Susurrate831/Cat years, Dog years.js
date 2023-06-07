bb83fd0cdbe83ac8861fb47cf74542d8c6e06284
smcdev <smcdev@outlook.com>
2023-06-07 17:54:42 -0500

    Create Cat years, Dog years.js

---
+++ Susurrate831/Cat years, Dog years.js
@@ -0,0 +1,16 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 24;
+    dogYears = 24;
+  } else {
+    catYears = (humanYears - 2) * 4 + 24;
+    dogYears = (humanYears - 2) * 5 + 24;
+  }
+
+  return [humanYears, catYears, dogYears];
+};
