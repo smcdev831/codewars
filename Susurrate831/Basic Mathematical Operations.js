e00df435fc0ff36a811abf72ba57bc262fd1567a
smcdev <smcdev@outlook.com>
2023-06-07 18:27:30 -0500

    Create Basic Mathematical Operations.js

---
+++ Susurrate831/Basic Mathematical Operations.js
@@ -0,0 +1,20 @@
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+      break;
+    case "-":
+      return value1 - value2;
+      break;
+    case "*":
+      return value1 * value2;
+      break;
+    case "/":
+      return value1 / value2;
+      break;
+
+    default:
+      return 0;
+      break;
+  }
+}
