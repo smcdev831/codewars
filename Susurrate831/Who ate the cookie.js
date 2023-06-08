b1a3ceed68f41cfe7fde31f6720e6bb6725d1064
smcdev <smcdev@outlook.com>
2023-06-07 19:41:18 -0500

    Create Who ate the cookie.js

---
+++ Susurrate831/Who ate the cookie.js
@@ -0,0 +1,7 @@
+function cookie(x) {
+  return typeof x === "string"
+    ? "Who ate the last cookie? It was Zach!"
+    : typeof x === "number"
+    ? "Who ate the last cookie? It was Monica!"
+    : `Who ate the last cookie? It was the dog!`;
+}
