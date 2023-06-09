4531bda081de94a29993b682677d40a59c30e3f8
smcdev <smcdev@outlook.com>
2023-06-08 23:16:54 -0500

    Create Freudian translator.js

---
+++ Susurrate831/Freudian translator.js
@@ -0,0 +1,12 @@
+function toFreud(string) {
+  if (!string) {
+    return "";
+  }
+
+  string = string.split(" ");
+  for (let i = 0; i < string.length; i++) {
+    string[i] = "sex";
+  }
+  string = string.join(" ");
+  return string;
+}
