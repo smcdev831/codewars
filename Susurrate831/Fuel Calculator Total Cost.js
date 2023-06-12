ebf31762af6f7b30a2d316125600ac7e64954e62
smcdev <smcdev@outlook.com>
2023-06-12 00:05:08 -0500

    Update Fuel Calculator Total Cost.js

--- Susurrate831/Fuel Calculator Total Cost.js
+++ Susurrate831/Fuel Calculator Total Cost.js
@@ -1,8 +1,8 @@
-function fuelPrice(litres, pricePerLiter) {
-  for (var i = 2; i <= 10; i += 2) {
+function fuelPrice(litres, pricePerLitre) {
+  for (let i = 2; i <= 10; i += 2) {
     if (litres >= i) {
-      pricePerLiter -= 0.05;
+      pricePerLitre -= 0.05;
     }
   }
-  return Math.round(litres * pricePerLiter * 100) / 100;
+  return Math.round(litres * pricePerLitre * 100) / 100;
 }
