2434315b27be370b4d4d34f71b0d30c756a4bec5
smcdev <smcdev@outlook.com>
2023-06-11 23:15:29 -0500

    Create Age Range Compatibility Equation.js

---
+++ Susurrate831/Age Range Compatibility Equation.js
@@ -0,0 +1,12 @@
+function datingRange(age) {
+  let min;
+  let max;
+  if (age <= 14) {
+    min = Math.floor(age - 0.1 * age);
+    max = Math.floor(age + 0.1 * age);
+  } else {
+    min = Math.floor(age / 2 + 7);
+    max = Math.floor((age - 7) * 2);
+  }
+  return `${min}-${max}`;
+}
