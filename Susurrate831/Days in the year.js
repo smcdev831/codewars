5814dd574278f057242ad125ab4b5b6ee954207d
smcdev <smcdev@outlook.com>
2023-06-13 21:27:37 -0500

    Update Days in the year.js

--- Susurrate831/Days in the year.js
+++ Susurrate831/Days in the year.js
@@ -1,3 +1,5 @@
 function yearDays(year) {
-  return year % 4 === 0 ? `${year} has 366 days` : `${year} has 365 days`;
+  return (!(year % 100) && year % 400) || year % 4
+    ? `${year} has 365 days`
+    : `${year} has 366 days`;
 }
