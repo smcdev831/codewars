c7409c13cff5d2b66bcc8fcbfbace03252e8d15e
smcdev <smcdev@outlook.com>
2023-06-14 17:16:34 -0500

    Create Tip Calculator.js

---
+++ Susurrate831/Tip Calculator.js
@@ -0,0 +1,23 @@
+function calculateTip(amount, rating) {
+  switch (rating.toLowerCase()) {
+    case "terrible":
+      return Math.ceil(amount * 0);
+      break;
+    case "poor":
+      return Math.ceil(amount * 0.05);
+      break;
+    case "good":
+      return Math.ceil(amount * 0.1);
+      break;
+    case "great":
+      return Math.ceil(amount * 0.15);
+      break;
+    case "excellent":
+      return Math.ceil(amount * 0.2);
+      break;
+
+    default:
+      return "Rating not recognised";
+      break;
+  }
+}
