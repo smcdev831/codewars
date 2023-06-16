feed9dc0d5c085e961e5f87480ef9c73f887c5f4
smcdev <smcdev@outlook.com>
2023-06-15 20:04:11 -0500

    Create Vowel remover.js

---
+++ Susurrate831/Vowel remover.js
@@ -0,0 +1,8 @@
+function shortcut(string) {
+  newString = string.split("a").join("");
+  newString = newString.split("e").join("");
+  newString = newString.split("i").join("");
+  newString = newString.split("o").join("");
+  newString = newString.split("u").join("");
+  return newString;
+}
