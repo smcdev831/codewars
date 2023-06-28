47bee27d4b6980c172aaa7e91f6d762bc79e9914
smcdev <smcdev@outlook.com>
2023-06-28 14:07:32 -0500

    Create Grasshopper - Create the rooms.js

---
+++ Susurrate831/Grasshopper - Create the rooms.js
@@ -0,0 +1,17 @@
+var rooms = {
+  room1: {
+    name: "Hall",
+    description: "Hall leading to cavern",
+    completed: "Y",
+  },
+  room2: {
+    name: "Cavern",
+    description: "Large open cavern",
+    completed: "N",
+  },
+  room3: {
+    name: "Secret Room",
+    description: "Hidden room in cavern",
+    completed: "N",
+  },
+};
