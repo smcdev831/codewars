1d6db469fec8d7285369ce731afdb6bc1cd1f776
smcdev <smcdev@outlook.com>
2023-07-01 23:20:18 -0500

    Update Twice as old.js

--- Susurrate831/Twice as old.js
+++ Susurrate831/Twice as old.js
@@ -1,3 +1,8 @@
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - 2 * sonYearsOld);
 }
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  ages = dadYearsOld - 2 * sonYearsOld;
+  return ages < 0 ? ages * -1 : ages;
+}
