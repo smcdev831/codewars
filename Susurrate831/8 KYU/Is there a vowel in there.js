40cb319c7957cae3374f697834ac53f43e193e9d
smcdev <smcdev@outlook.com>
2023-07-03 18:18:53 -0500

    Create Is there a vowel in there.js

---
+++ Susurrate831/8 KYU/Is there a vowel in there.js
@@ -0,0 +1,8 @@
+function isVow(a) {
+  for (var i = 0, l = a.length; i < l; ++i) {
+    var char = String.fromCharCode(a[i]);
+    if ("aeiou".indexOf(char) !== -1) a[i] = char;
+  }
+
+  return a;
+}
