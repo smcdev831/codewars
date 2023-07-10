0c9795eddcdb0e760443c24be4c4ab3ac12f6578
smcdev <smcdev@outlook.com>
2023-07-10 17:42:36 -0500

    Create Likes Vs Dislikes.js

---
+++ Susurrate831/7 KYU/Likes Vs Dislikes.js
@@ -0,0 +1,11 @@
+function likeOrDislike(buttons) {
+  let state = "Nothing";
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i] === state) {
+      state = "Nothing";
+    } else {
+      state = buttons[i];
+    }
+  }
+  return state;
+}
